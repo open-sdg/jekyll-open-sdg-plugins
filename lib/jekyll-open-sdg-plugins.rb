cdbf191e86cbef3230fd084e2ca100757c192411
Brock Fanning <brockfanning@gmail.com>
2019-02-22 12:12:37 -0500

    Remove a lot of plumbing for now

--- lib/jekyll-open-sdg-plugins.rb
+++ lib/jekyll-open-sdg-plugins.rb
@@ -1,5 +1,5 @@
 require_relative "jekyll-open-sdg-plugins/version"
-require_relative "jekyll-open-sdg-plugins/generator"
+require_relative "jekyll-open-sdg-plugins/multilingual_metadata"
 
 module JekyllOpenSdgPlugins
 end
