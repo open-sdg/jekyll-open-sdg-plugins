1d46eefa6b888b25f8070f545118442f2954adae
Brock Fanning <brockfanning@gmail.com>
2019-05-18 22:34:53 -0400

    Filter for getting indicator names

---
+++ lib/jekyll-open-sdg-plugins/indicator_name.rb
@@ -0,0 +1,94 @@
+require "jekyll"
+require_relative "helpers"
+
+module Jekyll
+  module IndicatorName
+
+    # Takes an indicator ID (dot-delimited or dash-delimited) and returns the
+    # translated indicator name (according to the current language). This lookup
+    # is as forgiving as possible, to make sure that something is always there.
+    #
+    # The order of preference in the lookup is:
+    #
+    # 1. "indicator_name" in translated metadata
+    # 2. "title" in translated metadata
+    # 3. if global, translated global indicator name
+    # 4. "indicator_name" in non-translated metadata
+    # 5. "title" in non-translated metadata
+    # 6. indicator ID
+    def indicator_name(inid)
+
+      # Safety code - abort now if id is nil.
+      if inid.nil?
+        return ""
+      end
+
+      # Also make sure it is a string, and otherwise just return it.
+      if not inid.is_a? String
+        return inid
+      end
+
+      # More safety code - abort now if inid is empty.
+      if inid.empty?
+        return ""
+      end
+
+      # Normalize around dash-delimited inids.
+      inid = inid.gsub('.', '-')
+
+      # Some variables to help our lookups later.
+      page = @context.environments.first['page']
+      language = page['language']
+      data = @context.registers[:site].data
+      translations = data['translations']
+      meta = data['meta'][inid]
+      metadata_fields = ['indicator_name', 'title']
+
+      # First choice, is there a subfolder translation of any metadata fields?
+      if meta and meta.has_key? language
+        metadata_fields.each do |field|
+          if meta[language].has_key? field
+            return meta[language][field]
+          end
+        end
+      end
+
+      # Next choice, are any of the metadata fields a translation key?
+      metadata_fields.each do |field|
+        if meta and meta.has_key? field
+          untranslated = meta[field]
+          translated = opensdg_translate_key(untranslated, translations, language)
+          if untranslated != translated
+            # If the opensdg_translate_key() function returned something else,
+            # that means it was an actual translation key.
+            return translated
+          end
+        end
+      end
+
+      # Next, is this a global indicator with a translation? For this we actually
+      # need the inid dot-delimited.
+      inid_dots = inid.gsub('-', '.')
+      if translations.has_key? language
+        if translations[language].has_key? 'global_indicators'
+          if translations[language]['global_indicators'].has_key? inid_dots
+            return translations[language]['global_indicators'][inid_dots]['title']
+          end
+        end
+      end
+
+      # Next just return any untranslated metadata field.
+      metadata_fields.each do |field|
+        if meta and meta.has_key? field
+          return meta[field]
+        end
+      end
+
+      # Still here? Just return the inid.
+      return inid
+
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::IndicatorName)
--- lib/jekyll-open-sdg-plugins/translate_key.rb
+++ lib/jekyll-open-sdg-plugins/translate_key.rb
@@ -1,4 +1,5 @@
 require "jekyll"
+require_relative "helpers"
 
 module Jekyll
   module TranslateKey
@@ -7,54 +8,11 @@ module Jekyll
     # key.
     def t(key)
 
-      # Safety code - abort now if key is nil.
-      if key.nil?
-        return ""
-      end
-
-      # Also make sure it is a string, and other just return it.
-      if not key.is_a? String
-        return key
-      end
-
-      # More safety code - abort now if key is empty.
-      if key.empty?
-        return ""
-      end
-
       # Determine the language of the current page.
       translations = @context.registers[:site].data['translations']
       language = @context.environments.first["page"]['language']
 
-      # Keep track of the last thing we drilled to.
-      drilled = translations[language]
-
-      # Keep track of how many levels we have drilled.
-      levels_drilled = 0
-      levels = key.split('.')
-
-      # Loop through each level.
-      levels.each do |level|
-
-        # If we have drilled down to a scalar value too soon, abort.
-        break if drilled.class != Hash
-
-        if drilled.has_key? level
-          # If we find something, continue drilling.
-          drilled = drilled[level]
-          levels_drilled += 1
-        end
-
-      end
-
-      # If we didn't drill the right number of levels, return the
-      # original string.
-      if levels.length != levels_drilled
-        return key
-      end
-
-      # Otherwise we must have drilled all they way.
-      return drilled
+      return opensdg_translate_key(key, translations, language)
     end
   end
 end
