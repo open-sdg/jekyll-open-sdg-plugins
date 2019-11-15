1df45724e6f8e248b17a77767fbcc0a6bab57c29
Brock Fanning <brockfanning@gmail.com>
2019-11-15 10:55:19 -0500

    Remove unneeded plugin and bump version

--- lib/jekyll-open-sdg-plugins.rb
+++ lib/jekyll-open-sdg-plugins.rb
@@ -5,7 +5,7 @@ require_relative "jekyll-open-sdg-plugins/translate_key"
 require_relative "jekyll-open-sdg-plugins/create_indicators"
 require_relative "jekyll-open-sdg-plugins/create_goals"
 require_relative "jekyll-open-sdg-plugins/create_pages"
-require_relative "jekyll-open-sdg-plugins/get_indicator_name"
+require_relative "jekyll-open-sdg-plugins/sdg_variables"
 
 module JekyllOpenSdgPlugins
 end
--- lib/jekyll-open-sdg-plugins/version.rb
+++ lib/jekyll-open-sdg-plugins/version.rb
@@ -1,3 +1,3 @@
 module JekyllOpenSdgPlugins
-  VERSION = "0.0.16".freeze
+  VERSION = "1.0.0-rc1".freeze
 end
