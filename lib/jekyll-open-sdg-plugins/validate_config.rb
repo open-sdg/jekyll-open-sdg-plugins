6355a9fac98f133095bb75bf4f0d6edac7610bf4
Brock Fanning <brockfanning@gmail.com>
2020-08-17 15:28:04 -0400

    Make validation an opt-in thing

--- lib/jekyll-open-sdg-plugins/validate_config.rb
+++ lib/jekyll-open-sdg-plugins/validate_config.rb
@@ -15,41 +15,44 @@ module JekyllOpenSdgPlugins
       schema = JSON.parse(json_from_file)
       schemer = JSONSchemer.schema(schema)
 
-      unless schemer.valid?(site.config)
-        errors = schemer.validate(site.config).to_a
-        errors.each do |error|
-          if error['type'] == 'required'
-            missing = []
-            error['schema']['required'].each do |required_property|
-              unless error['data'].has_key?(required_property)
-                message = 'Missing configuration setting: ' + required_property
-                if error['schema'].has_key?('title')
-                  message += ' (' + error['schema']['title'] + ')'
+      # Perform validation if the "validate_configuration" flag is true.
+      if site.config.has_key?('validate_configuration') && site.config['validate_configuration']
+        unless schemer.valid?(site.config)
+          errors = schemer.validate(site.config).to_a
+          errors.each do |error|
+            if error['type'] == 'required'
+              missing = []
+              error['schema']['required'].each do |required_property|
+                unless error['data'].has_key?(required_property)
+                  message = 'Missing configuration setting: ' + required_property
+                  if error['schema'].has_key?('title')
+                    message += ' (' + error['schema']['title'] + ')'
+                  end
+                  opensdg_notice(message)
                 end
-                opensdg_notice(message)
+              end
+            else
+              message = 'Validation error of type: ' + error['type']
+              if error['schema'] && error['schema'].has_key?('title')
+                message += ' (' + error['schema']['title'] + ')'
+              end
+              opensdg_notice(message)
+              if error['schema']
+                opensdg_notice('Expected schema:')
+                puts error['schema'].inspect
+              end
+              if error['data']
+                opensdg_notice('Actual data:')
+                puts error['data'].inspect
               end
             end
-          else
-            message = 'Validation error of type: ' + error['type']
-            if error['schema'] && error['schema'].has_key?('title')
-              message += ' (' + error['schema']['title'] + ')'
-            end
-            opensdg_notice(message)
-            if error['schema']
-              opensdg_notice('Expected schema:')
-              puts error['schema'].inspect
-            end
-            if error['data']
-              opensdg_notice('Actual data:')
-              puts error['data'].inspect
-            end
           end
+          opensdg_notice "The site configuration is not valid. See feedback above."
+          raise "Invalid site configuration"
         end
-        opensdg_notice "The site configuration is not valid. See feedback above."
-        raise "Invalid site configuration"
       end
 
-      # Also place the schema in site data so it can be used in Jekyll templates.
+      # Regardless place the schema in site data so it can be used in Jekyll templates.
       site.data['config-schema'] = schema
 
     end
