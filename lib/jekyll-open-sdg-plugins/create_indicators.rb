abc101376f324c93afa5722e64888c7970259023
Brock Fanning <brockfanning@gmail.com>
2021-02-07 12:02:55 -0500

    Create metadata config pages

--- lib/jekyll-open-sdg-plugins/create_indicators.rb
+++ lib/jekyll-open-sdg-plugins/create_indicators.rb
@@ -7,17 +7,21 @@ module JekyllOpenSdgPlugins
     priority :normal
 
     def generate(site)
+      # Some references to clean up the code below.
+      language_config = site.config['languages']
+      indicator_config = site.config['create_indicators']
+      form_config = site.config['create_config_forms']
       # If site.create_indicators is set, create indicators per the metadata.
-      if site.config['languages'] and site.config['create_indicators'] and site.config['create_indicators'].key?('layout') and site.config['create_indicators']['layout'] != ''
+      if language_config and indicator_config and indicator_config.key?('layout') and indicator_config['layout'] != ''
         # Decide what layout to use for the indicator pages.
-        layout = site.config['create_indicators']['layout']
+        layout = indicator_config['layout']
         # See if we need to "map" any language codes.
         languages_public = Hash.new
         if site.config['languages_public']
           languages_public = opensdg_languages_public(site)
         end
         # Loop through the languages.
-        site.config['languages'].each_with_index do |language, index|
+        language_config.each_with_index do |language, index|
           # Get the "public language" (for URLs) which may be different.
           language_public = language
           if languages_public[language]
@@ -45,24 +49,26 @@ module JekyllOpenSdgPlugins
             site.collections['indicators'].docs << IndicatorPage.new(site, site.source, dir, inid, language, layout)
           end
         end
-        # Create the indicator configuration pages.
-        if site.config['create_config_forms'] && site.config['create_config_forms'].key?('layout') && site.config['create_config_forms']['layout'] != ''
+        # Create the indicator settings configuration pages.
+        if form_config && form_config.key?('layout') && form_config['layout'] != ''
+
+          layout = form_config['layout']
           metadata = {}
-          layout = site.config['create_config_forms']['layout']
           if opensdg_translated_builds(site)
             if site.data.has_key?('untranslated')
               metadata = site.data['untranslated']['meta']
             else
-              default_language = site.config['languages'][0]
+              default_language = language_config[0]
               metadata = site.data[default_language]['meta']
             end
           else
             metadata = site.data['meta']
           end
+
           # Loop through the indicators (using metadata as a list).
           if !metadata.empty?
             # Loop through the languages.
-            site.config['languages'].each_with_index do |language, index|
+            language_config.each_with_index do |language, index|
               # Get the "public language" (for URLs) which may be different.
               language_public = language
               if languages_public[language]
@@ -81,12 +87,42 @@ module JekyllOpenSdgPlugins
               end
             end
           end
+
+          # Create the indicator metadata configuration pages.
+          scopes = []
+          if form_config.key?('metadata_scopes') && form_config['metadata_scopes'].length() > 0
+            scopes = form_config['metadata_scopes']
+          end
+          scopes.each do |scope|
+            # Loop through the indicators (using metadata as a list).
+            if !metadata.empty?
+              # Loop through the languages.
+              language_config.each_with_index do |language, index|
+                # Get the "public language" (for URLs) which may be different.
+                language_public = language
+                if languages_public[language]
+                  language_public = languages_public[language]
+                end
+                metadata.each do |inid, meta|
+                  permalink = inid
+                  if meta.has_key?('permalink') and meta['permalink'] != ''
+                    permalink = meta['permalink']
+                  end
+                  dir = File.join('config', scope['key'], permalink)
+                  if index != 0
+                    dir = File.join(language_public, 'config', scope['key'], permalink)
+                  end
+                  site.collections['pages'].docs << IndicatorMetadataPage.new(site, site.source, dir, inid, language, meta, layout, scope)
+                end
+              end
+            end
+          end
         end
       end
     end
   end
 
-  # A Page subclass used in the `CreateIndicators` class.
+  # A Page subclass used in the `CreateIndicators` class for the indicators.
   class IndicatorPage < Jekyll::Page
     def initialize(site, base, dir, inid, language, layout)
       @site = site
@@ -104,7 +140,7 @@ module JekyllOpenSdgPlugins
     end
   end
 
-  # A Page subclass used in the `CreateIndicators` class.
+  # A Page subclass used in the `CreateIndicators` class for the indicator config forms.
   class IndicatorConfigPage < Jekyll::Page
     def initialize(site, base, dir, inid, language, meta, layout)
       @site = site
@@ -120,6 +156,27 @@ module JekyllOpenSdgPlugins
       self.data['layout'] = layout
       self.data['meta'] = meta
       self.data['title'] = 'Open SDG indicator configuration: ' + self.data['indicator_number']
+      self.data['config_filename'] = inid + '.yml'
+    end
+  end
+
+  # A Page subclass used in the `CreateIndicators` class for the metadata config forms.
+  class IndicatorMetadataPage < Jekyll::Page
+    def initialize(site, base, dir, inid, language, meta, layout, scope)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.data = {}
+      self.data['language'] = language
+      self.data['indicator_number'] = inid.gsub('-', '.')
+      self.data['config_type'] = 'metadata'
+      self.data['metadata_scope'] = scope['key']
+      self.data['layout'] = layout
+      self.data['meta'] = meta
+      self.data['title'] = 'Open SDG ' + scope['label'] + 'metadata: ' + self.data['indicator_number']
       self.data['config_filename'] = inid + '.yml'
     end
   end
---
+++ lib/jekyll-open-sdg-plugins/metadata_schema_to_config.rb
@@ -0,0 +1,61 @@
+require "jekyll"
+require_relative "helpers"
+
+module JekyllOpenSdgPlugins
+  class MetadataSchemaToConfig < Jekyll::Generator
+    safe true
+    priority :lowest
+
+    def generate(site)
+
+      # Convert the metadata schema from the internal format into JSONSchema so
+      # it can be used to create config forms.
+
+      language_config = site.config['languages']
+      form_config = site.config['create_config_forms']
+      t = site.data['translations']
+      lang = language_config[0]
+
+      opensdg_translate_key(key, translations, language)
+
+      if form_config && form_config.key?('metadata_scopes') && form_config['metadata_scopes'].length() > 0
+
+        schema = {}
+        scopes = []
+        form_config['metadata_scopes'].each do |scope|
+          schema[scope['key']] = {
+            "type" => "object",
+            "title" => "Open SDG " + scope['label'] + " metadata",
+            "properties" => {},
+          }
+          scopes.append(scope['key'])
+        end
+
+        site.data.schema.each do |field|
+          field_scope = field['field']['scope']
+          next unless scopes.include?(field_scope)
+
+          field_name = field['name']
+          to_translate = field_name
+          if field['field'].has_key?('translation_key')
+            to_translate = field['field']['translation_key']
+          elsif t[lang].has_key?('metadata_fields') && t[lang]['metadata_fields'].has_key?(field_name)
+            to_translate = 'metadata_fields.' + field_name
+          elsif field['field'].has_key?('label')
+            to_translate = field['field']['label']
+          end
+          field_label = opensdg_translate_key(to_translate, t, lang)
+
+          schema[field_scope]['properties'][field_name] = {
+            "type" => "string",
+            "format" => "markdown",
+            "title" => field_label,
+          }
+        end
+
+        # Regardless place the schema in site data so it can be used in Jekyll templates.
+        site.data['schema-indicator-metadata'] = schema
+      end
+    end
+  end
+end
