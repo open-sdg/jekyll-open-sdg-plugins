b94b00c70218113096d09023c869f523dff2c9cf
Brock Fanning <brockfanning@gmail.com>
2021-05-09 21:40:23 -0400

    Logic fix

--- lib/jekyll-open-sdg-plugins/sdg_variables.rb
+++ lib/jekyll-open-sdg-plugins/sdg_variables.rb
@@ -387,8 +387,8 @@ module JekyllOpenSdgPlugins
             doc.data['remote_data_prefix'] = normalize_baseurl(baseurl)
           end
           if opensdg_translated_builds(site)
-            doc.data['remote_data_prefix'] = File.join(doc.data['remote_data_prefix'], language)
             doc.data['remote_data_prefix_untranslated'] = File.join(doc.data['remote_data_prefix'], 'untranslated')
+            doc.data['remote_data_prefix'] = File.join(doc.data['remote_data_prefix'], language)
           else
             doc.data['remote_data_prefix_untranslated'] = doc.data['remote_data_prefix']
           end
