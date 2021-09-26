bc255a67cde508b1ba7e46c24301fa14b7092ae3
Brock Fanning <brockfanning@gmail.com>
2021-09-26 13:50:08 -0400

    Tweaks

--- lib/jekyll-open-sdg-plugins/backwards_compatibility.rb
+++ lib/jekyll-open-sdg-plugins/backwards_compatibility.rb
@@ -10,7 +10,8 @@ module JekyllOpenSdgPlugins
       statuses.each do |status|
         status_in_site_config = site.config['reporting_status']['status_types'].detect {|s| s['value'] == status['status'] }
         if status_in_site_config.nil?
-          opensdg_notice('Unexpected reporting status type: ' + status['status'])
+          opensdg_notice('Unexpected reporting status type: ' + status['status'] + '. Expected reporting status types:'
+          puts site.config['reporting_status']['status_types']
         end
         status['translation_key'] = status_in_site_config['label']
       end
