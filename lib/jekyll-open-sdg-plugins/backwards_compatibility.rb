de3888346aa5f8c209b70b7d3e80facfa2acfc1e
Brock Fanning <brockfanning@gmail.com>
2021-09-26 11:02:41 -0400

    Debugging

--- lib/jekyll-open-sdg-plugins/backwards_compatibility.rb
+++ lib/jekyll-open-sdg-plugins/backwards_compatibility.rb
@@ -18,7 +18,7 @@ module JekyllOpenSdgPlugins
             reporting_status = site.data['schema'].detect {|f| f['name'] == 'reporting_status' }
             reporting_status_types = reporting_status['field']['options']
             site.config['reporting_status']['status_types'] = reporting_status_types.map do |status_type|
-              return {
+              {
                 'value': status_type['value'],
                 'label': status_type['translation_key'],
               }
