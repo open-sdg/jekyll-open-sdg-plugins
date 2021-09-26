5542dc1c0617754bb3c1374f532ed3672fb27eb7
Brock Fanning <brockfanning@gmail.com>
2021-09-26 10:48:59 -0400

    Tweaks

--- lib/jekyll-open-sdg-plugins/backwards_compatibility.rb
+++ lib/jekyll-open-sdg-plugins/backwards_compatibility.rb
@@ -14,8 +14,14 @@ module JekyllOpenSdgPlugins
         unless site.config.has_key?('reporting_status') &&
                site.config['reporting_status'].has_key?('status_types') &&
                site.config['reporting_status']['status_types'].count > 0
-            legacy_reporting_status_types = site.data['schema']
-            puts legacy_reporting_status_types
+            reporting_status = site.data['schema'].detect {|f| f['name'] == 'reporting_status' }
+            reporting_status_types = reporting_status['field']['options']
+            site.config['reporting_status']['status_types'] = reporting_status_types.map do |status_type|
+              return {
+                'value': status_type['value'],
+                'label': status_type['translation_key'],
+              }
+            end
         end
     end
   end
