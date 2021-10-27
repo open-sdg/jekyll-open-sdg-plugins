19ceddd45fb4296faa5a8931a07c8f5a2d72e96b
brockfanning <brockfanning@gmail.com>
2021-10-27 09:53:08 -0400

    Avoid config changes without user action (#114)
    * Avoid config changes without user action
    
    * Also remove special treatment of repository_link in site config
    
    * Make metadata_edit_url and data_edit_url optional
    
    * Revert change from the 'edit_url' settings

--- lib/jekyll-open-sdg-plugins/create_pages.rb
+++ lib/jekyll-open-sdg-plugins/create_pages.rb
@@ -81,15 +81,6 @@ module JekyllOpenSdgPlugins
         # Make sure the form settings are set.
         config_page = pages.find { |page| page['layout'] == 'config-builder' }
         if config_page != nil && form_settings && form_settings['enabled']
-          # Special treatment of repository_link.
-          if form_settings['repository_link'] && form_settings['repository_link'] != ''
-            unless form_settings['repository_link'].start_with?('http')
-              repo_url = site.config['repository_url_site']
-              if repo_url && repo_url != '' && repo_url.start_with?('http')
-                form_settings['repository_link'] = repo_url + form_settings['repository_link']
-              end
-            end
-          end
           config_page['form_settings'] = form_settings
         end
 
