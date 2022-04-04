37c7b48520c89fd6cafed932ad62b32643d81e2c
Brock Fanning <brockfanning@gmail.com>
2022-04-04 10:08:33 -0400

    Save routes for zips, data, images, and pages

---
+++ lib/jekyll-open-sdg-plugins/routes.rb
@@ -0,0 +1,87 @@
+require "jekyll"
+require_relative "helpers"
+
+module JekyllOpenSdgPlugins
+  class Routes < Jekyll::Generator
+    safe true
+    priority :lowest
+
+    def generate(site)
+
+      routes = {}
+      baseurl = ''
+      if site.config.has_key?('baseurl')
+        baseurl = site.config['baseurl']
+      end
+      unless baseurl.end_with?('/')
+        baseurl = baseurl + '/'
+      end
+
+      routes['pages'] = []
+      site.collections.keys.each do |collection|
+        site.collections[collection].docs.each do |doc|
+          route = baseurl + doc.url
+          route = route.gsub('//', '/')
+          routes['pages'].append(route)
+        end
+      end
+
+      routes['images'] = []
+      if site.config.has_key?('logos') and site.config['logos'].length > 0
+        site.config['logos'].each do |logo|
+          route = baseurl + logo['src']
+          routes['images'].append(route)
+        end
+      end
+
+      goal_image_base = 'https://open-sdg.org/sdg-translations/assets/img/goals'
+      if site.config.has_key?('goal_image_base')
+        goal_image_base = site.config['goal_image_base']
+      end
+      goal_image_extension = 'png'
+      if site.config.has_key?('goal_image_extension') && site.config['goal_image_extension'] != ''
+        goal_image_extension = site.config['goal_image_extension']
+      end
+      goal_image_base_contrast = goal_image_base.gsub('img/goals', 'img/high-contrast/goals')
+
+      site.collections['goals'].docs.each do |doc|
+        goal_number = doc.data['goal']['number']
+        language = doc.data['language']
+        if goal_number.is_a? Numeric
+          goal_number = goal_number.to_s
+        end
+        route = goal_image_base + '/' + language + '/' + goal_number + '.' + goal_image_extension
+        routes['images'].append(route)
+        unless goal_image_extension == 'svg'
+          route = goal_image_base_contrast + '/' + language + '/' + goal_number + '.' + goal_image_extension
+          routes['images'].append(route)
+        end
+      end
+
+      data_prefix = site.config['remote_data_prefix']
+      unless data_prefix.start_with?('http')
+        data_prefix = baseurl
+      end
+      unless data_prefix.end_with?('/')
+        data_prefix = data_prefix + '/'
+      end
+
+      routes['json'] = []
+      routes['csv'] = []
+      routes['zip'] = []
+      site.config['languages'].each do |language|
+        site.collections['indicators'].docs.each do |doc|
+          route = data_prefix + language + '/comb/' + doc.data['indicator']['slug'] + '.json'
+          routes['json'].append(route)
+          route = data_prefix + language + '/data/' + doc.data['indicator']['slug'] + '.csv'
+          routes['csv'].append(route)
+        end
+        route = data_prefix + language + '/zip/' + site.data[language]['zip']['filename']
+        routes['zip'].append(route)
+      end
+      
+      site.data['routes'] = routes
+      puts routes
+    end
+  end
+end
