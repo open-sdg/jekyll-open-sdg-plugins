0586148d80d72adf63114b8bbe031a5107e55c35
brockfanning <brockfanning@gmail.com>
2022-05-17 09:17:15 -0400

    Parse site config and translation keys within translations (#130)
    * Support % parameters inside of content that can be site config or translation keys
    
    * Account for paragraph tags
    
    * Debugging
    
    * Debugging

--- lib/jekyll-open-sdg-plugins/translate_key.rb
+++ lib/jekyll-open-sdg-plugins/translate_key.rb
@@ -9,10 +9,36 @@ module Jekyll
     def t(key)
 
       # Determine the language of the current page.
-      translations = @context.registers[:site].data['translations']
+      site = @context.registers[:site]
+      translations = site.data['translations']
       language = @context.environments.first["page"]['language']
+      translated = opensdg_translate_key(key, translations, language)
+      # Also look for references to site configuration within the translation,
+      # as "%" parameters.
+      translated = translated.gsub(/%([a-zA-Z0-9_.\-]+)/) do |match|
+        # Special case for trailing dots.
+        trailing_dot = match.end_with?('.')
+        key_candidate = match.delete_suffix('.').delete_prefix('%')
+        # Check to see if it is a site configuration.
+        translated_word = opensdg_parse_site_config(key_candidate, site)
+        # Check to see if the value of the site config may have been
+        # a translation key itself. But do a safety check to avoid
+        # infinite loops.
+        if translated_word != key
+          translated_word = opensdg_translate_key(translated_word, translations, language)
+        end
+        # Replace the word if something changed.
+        if key_candidate != translated_word
+          match = translated_word
+          # Making sure to add back any trailing dots.
+          if trailing_dot
+            match = match + '.'
+          end
+        end
+        match
+      end
 
-      return opensdg_translate_key(key, translations, language)
+      return translated
     end
   end
 end
